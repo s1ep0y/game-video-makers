2d12c11fc90d115c34fb985dcbf1eee40de3ddbd
Danila Molodtsov <danmol04@gmail.com>
2020-07-21 18:58:17 +0300

    real init commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+import './styles/header.scss'
+import './styles/navbar.scss'
+// import "antd/dist/antd.css";
+import '@csstools/normalize.css';
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      <p>wrap content</p>
+    </div>
+  );
+}
+
+export default App;
