54677635a387de09657ee9aec9e8a598031d7c42
Danila Molodtsov <danmol04@gmail.com>
2020-07-21 20:00:45 +0300

    added fonts

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.scss';
 import './styles/header.scss'
 import './styles/navbar.scss'
+import './styles/main.scss'
 // import "antd/dist/antd.css";
 import '@csstools/normalize.css';
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+
 
 function App() {
   return (
